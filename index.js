96e7139e2eaa1d0385cdf55bf97a8f614cd52b55
Sepehr Tavakoli <sepehrtvk@aut.ac.ir>
2020-09-28 00:29:30 +0330

    Use templates.

--- index.js
+++ index.js
@@ -14,7 +14,18 @@ const server = http.createServer((req, res) => {
 
      } else if (pathName === '/laptop' && id<labTopData.length){
         res.writeHead(200,{'Content-Type':'text/html'});
-        res.end('laptop');
+        fs.readFile(`${__dirname}/templates/template-laptop.html`,'utf8',(err , data)=> {
+            const laptop = labTopData[id];
+             output = data.replace(/{%PRODUCTNAME%}/g,laptop.productName);
+             output = output.replace(/{%IAMGE%}/g,laptop.image);
+             output = output.replace(/{%PRICE%}/g,laptop.price);
+             output = output.replace(/{%SCREEN%}/g,laptop.screen);
+             output = output.replace(/{%CPU%}/g,laptop.cpu);
+             output = output.replace(/{%STORAGE%}/g,laptop.storage);
+             output = output.replace(/{%RAM%}/g,laptop.ram);
+             output = output.replace(/{%DESCRIPTION%}/g,laptop.description);
+            res.end(output);
+        });
 
      } else {
          res.writeHead(404,{'Content-Type':'text/html'});
