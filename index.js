254d03749cc16a7d05290a966e7a66e0ad73b889
Sepehr Tavakoli <sepehrtvk@aut.ac.ir>
2020-09-27 21:55:35 +0330

    Make server part and products.

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 const json = fs.readFileSync(`${__dirname}/data/data.json`,'utf8');
 const labTopData = JSON.parse(json);
+
 const server = http.createServer((req, res) => {
-    
+     const pathName = url.parse(req.url,true).pathname;
+     const id = url.parse(req.url,true).query.id;
+
+     if(pathName === '/products' || pathName === '/' ){
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.end('Products');
+
+     } else if (pathName === '/laptop' && id<labTopData.length){
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.end('laptop');
+
+     } else {
+         res.writeHead(404,{'Content-Type':'text/html'});
+         res.end('URL was not in the server');
+     }
 });
+
+server.listen(1337,'127.0.0.1',() => {
+
+})
