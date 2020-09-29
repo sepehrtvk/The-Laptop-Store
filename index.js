68576dfeb4cc737e203fb0198d2e285d6c30ae74
Sepehr Tavakoli <sepehrtvk@aut.ac.ir>
2020-09-29 15:17:12 +0330

    Show all products in store.

--- index.js
+++ index.js
@@ -10,20 +10,22 @@ const server = http.createServer((req, res) => {
 
      if(pathName === '/products' || pathName === '/' ){
         res.writeHead(200,{'Content-Type':'text/html'});
-        res.end('Products');
+
+        fs.readFile(`${__dirname}/templates/template-overview.html`,'utf8',(err , data)=> {
+            let overviewOutput = data;
+            fs.readFile(`${__dirname}/templates/template-card.html`,'utf8',(err , data)=> {
+                const cardsOutput = labTopData.map(el => replaceTemplate(data,el)).join('');
+                overviewOutput= overviewOutput.replace('{%CARDS%}',cardsOutput);
+                res.end(overviewOutput);
+            });
+    
+        });
 
      } else if (pathName === '/laptop' && id<labTopData.length){
         res.writeHead(200,{'Content-Type':'text/html'});
         fs.readFile(`${__dirname}/templates/template-laptop.html`,'utf8',(err , data)=> {
             const laptop = labTopData[id];
-             output = data.replace(/{%PRODUCTNAME%}/g,laptop.productName);
-             output = output.replace(/{%IAMGE%}/g,laptop.image);
-             output = output.replace(/{%PRICE%}/g,laptop.price);
-             output = output.replace(/{%SCREEN%}/g,laptop.screen);
-             output = output.replace(/{%CPU%}/g,laptop.cpu);
-             output = output.replace(/{%STORAGE%}/g,laptop.storage);
-             output = output.replace(/{%RAM%}/g,laptop.ram);
-             output = output.replace(/{%DESCRIPTION%}/g,laptop.description);
+            const output = replaceTemplate(data,laptop);
             res.end(output);
         });
 
@@ -35,4 +37,17 @@ const server = http.createServer((req, res) => {
 
 server.listen(1337,'127.0.0.1',() => {
 
-})
+});
+
+function replaceTemplate(originalHTML , laptop){
+    output = originalHTML.replace(/{%PRODUCTNAME%}/g,laptop.productName);
+    output = output.replace(/{%IAMGE%}/g,laptop.image);
+    output = output.replace(/{%PRICE%}/g,laptop.price);
+    output = output.replace(/{%SCREEN%}/g,laptop.screen);
+    output = output.replace(/{%CPU%}/g,laptop.cpu);
+    output = output.replace(/{%STORAGE%}/g,laptop.storage);
+    output = output.replace(/{%RAM%}/g,laptop.ram);
+    output = output.replace(/{%DESCRIPTION%}/g,laptop.description);
+    output = output.replace(/{%ID%}/g,laptop.id);
+    return output;
+};
